ece39775745bc76600e86fe8a08f2bf46466006d
psegura- <psegura-@student.42madrid.com>
2023-01-05 12:21:18 +0100

    First commit

---
+++ philo/inc/philo.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psegura- <psegura-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/05 12:18:26 by psegura-          #+#    #+#             */
+/*   Updated: 2023/01/05 12:20:33 by psegura-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+//Write, Read
+# include <unistd.h>
+
+//Printf
+# include <stdio.h>
+
+//Malloc, Free, Exit
+# include <stdlib.h>
+
+#endif
+
+memset, printf, malloc, free, write,
+usleep, gettimeofday, pthread_create,
+pthread_detach, pthread_join, pthread_mutex_init,
+pthread_mutex_destroy,
